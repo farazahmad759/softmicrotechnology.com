61a0554aade98a1e7ddb165f720a24e871a0fbae
farazahmad759 <farazahmad759@gmail.com>
2020-04-07 21:38:13 +0500

    services page setup

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
+Route::get('/services', function () {
+    return view('services');
+});
 Route::get('/about', function () {
     return view('about');
 });
