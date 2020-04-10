357ebd314d54b332aa5b40738fcbf8a24f262fe6
farazahmad759 <farazahmad759@gmail.com>
2020-04-10 21:22:20 +0500

    Changed top menu

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,22 +1,40 @@
-<header id="header" class="transparent-header full-header" data-sticky-class="not-dark">
+<!-- Header
+		============================================= -->
+<header id="header" class="sticky-style-2">
+
+    <div class="container clearfix">
+
+        <!-- Logo
+				============================================= -->
+        <div id="logo">
+            <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png"
+                    alt="Canvas Logo"></a>
+            <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img
+                    src="images/logo@2x.png" alt="Canvas Logo"></a>
+        </div><!-- #logo end -->
+
+        <ul class="header-extras">
+            <li>
+                <i class="i-plain icon-email3 nomargin"></i>
+                <div class="he-text">
+                    Drop an Email
+                    <span>info@canvas.com</span>
+                </div>
+            </li>
+            <li>
+                <i class="i-plain icon-call nomargin"></i>
+                <div class="he-text">
+                    Get in Touch
+                    <span>1800-1144-551</span>
+                </div>
+            </li>
+        </ul>
+
+    </div>
 
     <div id="header-wrap">
 
-        <div class="container clearfix">
-
-            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
-
-            <!-- Logo
-        ============================================= -->
-            <div id="logo">
-                <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img
-                        src="images/logo.png" alt="Canvas Logo"></a>
-                <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img
-                        src="images/logo@2x.png" alt="Canvas Logo"></a>
-            </div><!-- #logo end -->
-
-            @include('layout/partials.nav')
-        </div>
+        @include('layout/partials.nav')
 
     </div>
 
--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,64 +1,115 @@
-            <!-- Primary Navigation
-        ============================================= -->
-            <nav id="primary-menu">
+				<!-- Primary Navigation
+				============================================= -->
+				<nav id="primary-menu" class="style-2">
 
-                <ul>
-                    <li class="">
-                        <a href="/">
-                            <div>Home</div>
-                        </a>
-                    </li>
-                    <li class=""><a href="/services">
-                            <div>Services</div>
-                        </a>
-                    </li>
-                    <li><a href="/products">
-                            <div>Products</div>
-                        </a>
-                        <ul>
-                            <li>
-                                <a href="#">
-                                    <div><i class="icon-stack"></i>Sliders</div>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <div>Revolution Slider</div>
-                                        </a>
-                                        <ul>
-                                            <li><a href="#">
-                                                    <div>Premium Templates</div>
-                                                </a></li>
-                                            <li><a href="#">
-                                                    <div>Full Screen</div>
-                                                </a></li>
-                                            <li><a href="#">
-                                                    <div>Full Width</div>
-                                                </a></li>
-                                            <li><a href="#">
-                                                    <div>Kenburns Effect</div>
-                                                </a></li>
-                                            <li><a href="#">
-                                                    <div>HTML5 Video</div>
-                                                </a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">
-                                            <div>Nivo Slider</div>
-                                        </a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class=""><a href="/contact">
-                            <div>Contact Us</div>
-                        </a>
-                    </li>
-                    <li class=""><a href="/about">
-                            <div>About Us</div>
-                        </a>
-                    </li>
-                </ul>
+				    <div class="container clearfix">
 
+				        <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
 
-            </nav><!-- #primary-menu end -->
+				        <ul>
+				            <li class="">
+				                <a href="/">
+				                    <div>Home</div>
+				                </a>
+				            </li>
+				            <li class=""><a href="/services">
+				                    <div>Services</div>
+				                </a>
+				            </li>
+				            <li><a href="/products">
+				                    <div>Products</div>
+				                </a>
+				                <ul>
+				                    <li>
+				                        <a href="#">
+				                            <div><i class="icon-stack"></i>Sliders</div>
+				                        </a>
+				                        <ul>
+				                            <li>
+				                                <a href="#">
+				                                    <div>Revolution Slider</div>
+				                                </a>
+				                                <ul>
+				                                    <li><a href="#">
+				                                            <div>Premium Templates</div>
+				                                        </a></li>
+				                                    <li><a href="#">
+				                                            <div>Full Screen</div>
+				                                        </a></li>
+				                                    <li><a href="#">
+				                                            <div>Full Width</div>
+				                                        </a></li>
+				                                    <li><a href="#">
+				                                            <div>Kenburns Effect</div>
+				                                        </a></li>
+				                                    <li><a href="#">
+				                                            <div>HTML5 Video</div>
+				                                        </a></li>
+				                                </ul>
+				                            </li>
+				                            <li><a href="#">
+				                                    <div>Nivo Slider</div>
+				                                </a></li>
+				                        </ul>
+				                    </li>
+				                </ul>
+				            </li>
+				            <li class=""><a href="/contact">
+				                    <div>Contact Us</div>
+				                </a>
+				            </li>
+				            <li class=""><a href="/about">
+				                    <div>About Us</div>
+				                </a>
+				            </li>
+				        </ul>
+
+				        <!-- Top Cart
+						============================================= -->
+				        <div id="top-cart">
+				            <a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>5</span></a>
+				            <div class="top-cart-content">
+				                <div class="top-cart-title">
+				                    <h4>Shopping Cart</h4>
+				                </div>
+				                <div class="top-cart-items">
+				                    <div class="top-cart-item clearfix">
+				                        <div class="top-cart-item-image">
+				                            <a href="#"><img src="images/shop/small/1.jpg" alt="Blue Round-Neck Tshirt" /></a>
+				                        </div>
+				                        <div class="top-cart-item-desc">
+				                            <a href="#">Blue Round-Neck Tshirt</a>
+				                            <span class="top-cart-item-price">$19.99</span>
+				                            <span class="top-cart-item-quantity">x 2</span>
+				                        </div>
+				                    </div>
+				                    <div class="top-cart-item clearfix">
+				                        <div class="top-cart-item-image">
+				                            <a href="#"><img src="images/shop/small/6.jpg" alt="Light Blue Denim Dress" /></a>
+				                        </div>
+				                        <div class="top-cart-item-desc">
+				                            <a href="#">Light Blue Denim Dress</a>
+				                            <span class="top-cart-item-price">$24.99</span>
+				                            <span class="top-cart-item-quantity">x 3</span>
+				                        </div>
+				                    </div>
+				                </div>
+				                <div class="top-cart-action clearfix">
+				                    <span class="fleft top-checkout-price">$114.95</span>
+				                    <button class="button button-3d button-small nomargin fright">View Cart</button>
+				                </div>
+				            </div>
+				        </div><!-- #top-cart end -->
+
+				        <!-- Top Search
+						============================================= -->
+				        <div id="top-search">
+				            <a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+				            <form action="search.html" method="get">
+				                <input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+				            </form>
+				        </div><!-- #top-search end -->
+
+				    </div>
+
+				</nav><!-- #primary-menu end -->
